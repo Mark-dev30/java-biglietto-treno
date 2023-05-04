47844431be0e3ea55992825bc421a1fdb6b26002
Mark-dev30 <fronterremarco@gmail.com>
2023-05-04 15:12:37 +0200

    round number

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,11 +1,13 @@
 package org.lessons.java;
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class CalcolaBiglietto {
 	public static void main(String[] args) {
 		Double priceKm = 0.21;
 		Scanner in = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.00");
 		System.out.println("Inserisci il numero di chilometri da percorrere");
 		Integer kilometres = Integer.parseInt(in.nextLine());
 		System.out.println("Inserisci la tua età");
@@ -14,12 +16,12 @@ public class CalcolaBiglietto {
 		if (age < 18) {
 			double discount = price * 20 / 100;
 			double finalPrice = price - discount;
-			System.out.println("Il totale da pagare è" + finalPrice + '€');
+			System.out.println("Il totale da pagare è " + df.format(finalPrice) + '€');
 		}
 		else if (age >= 65) {
 			double discount = price * 40 / 100;
 			double finalPrice = price - discount;
-			System.out.println("Il totale da pagare è" + finalPrice + '€');
+			System.out.println("Il totale da pagare è " + df.format(finalPrice) + '€');
 			
 		}
 		else {
